ca0e52ddeccdb135c3cbbd7dc8da66a62ba03103
Gibin <gibinmgeorge2001@gmail.com>
2024-03-04 20:09:49 -0600

    structured

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+import fs from 'fs';
+import inquirer from 'inquirer';
 
 // Function to prompt the user for input
-function promptUser() {
+const promptUser = () => {
   return inquirer.prompt([
     {
       type: 'input',
       name: 'text',
       message: 'Enter three characters for the text:',
-      validate: function(input) {
-        return input.length <= 3 ? true : 'Please enter up to three characters.';
-      }
+      validate: input => input.length <= 3 ? true : 'Please enter up to three characters.'
     },
     {
       type: 'input',
@@ -29,42 +27,42 @@ function promptUser() {
       message: 'Enter the shape color :'
     }
   ]);
-}
+};
 
 // Function to generate the SVG code based on user input
-function generateSVG(data) {
-    return `<svg width="300" height="200">
-      <rect width="100%" height="100%" fill="${data.shapeColor}" />
-      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="48" fill="${data.textColor}">${data.text}</text>
-    </svg>`;
-}
-  
+const generateSVG = data => {
+  return `<svg width="300" height="200">
+    <rect width="100%" height="100%" fill="${data.shapeColor}" />
+    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="48" fill="${data.textColor}">${data.text}</text>
+  </svg>`;
+};
+
 // Function to write the SVG code to a file
-function writeSVGToFile(svg) {
-    fs.writeFile('examples/logo.svg', svg, err => { // Save inside the examples folder
-      if (err) {
-        console.error('Error writing SVG file:', err);
-      } else {
-        console.log('Generated logo.svg');
-      }
-    });
-}
-  
-// Main function
-async function main() {
-    try {
-      // Prompt the user for input
-      const userInput = await promptUser();
-  
-      // Generate the SVG code
-      const svg = generateSVG(userInput);
-  
-      // Write the SVG code to a file
-      writeSVGToFile(svg);
-    } catch (error) {
-      console.error('Error:', error);
+const writeSVGToFile = svg => {
+  fs.writeFile('example/logo.svg', svg, err => { 
+    if (err) {
+      console.error('Error writing SVG file:', err);
+    } else {
+      console.log('Generated logo.svg');
     }
-}
-  
+  });
+};
+
+// Main function
+const main = async () => {
+  try {
+    // Prompt the user for input
+    const userInput = await promptUser();
+
+    // Generate the SVG code
+    const svg = generateSVG(userInput);
+
+    // Write the SVG code to a file
+    writeSVGToFile(svg);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
 // Call the main function
 main();
